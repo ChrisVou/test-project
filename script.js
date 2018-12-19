2a4723bffb980d62be221c2809b845226315dc78
unknown <ChrisVou@hotmail.com>
2018-12-19 19:11:26 +0200

    adding starting project

---
+++ script.js
@@ -0,0 +1,15 @@
+const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+	(a, b) => a.concat(b), []);
+
+const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+	(accumulator, array) => {
+		console.log('array', array);
+		console.log('accumulator', accumulator);
+		return accumulator.concat(array) 
+	}, []); 
+
+const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+	(accumulator, array) => {
+		debugger;
+		return accumulator.concat(array) 
+	}, []); 
