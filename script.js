9850245aa5671926b2c813fe21a86c3c74f988a1
unknown <ChrisVou@hotmail.com>
2018-12-25 16:40:10 +0200

    new changes

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
-	(a, b) => a.concat(b), []);
+var _ = require('lodash');
 
-const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
-	(accumulator, array) => {
-		console.log('array', array);
-		console.log('accumulator', accumulator);
-		return accumulator.concat(array) 
-	}, []); 
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(array, 3));
 
-const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
-	(accumulator, array) => {
-		debugger;
-		return accumulator.concat(array) 
-	}, []); 
+// const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+// 	(a, b) => a.concat(b), []);
+
+// const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+// 	(accumulator, array) => {
+// 		console.log('array', array);
+// 		console.log('accumulator', accumulator);
+// 		return accumulator.concat(array) 
+// 	}, []); 
+
+// const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+// 	(accumulator, array) => {
+// 		debugger;
+// 		return accumulator.concat(array) 
+// 	}, []); 
